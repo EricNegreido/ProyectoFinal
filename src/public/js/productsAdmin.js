7db783c4e0ec0db60c74f05f1e1dfc105af5ea7e
Eric Negreido <ericnegreidooo@gmail.com>
2024-02-07 22:47:32 -0300

    eventos

--- src/public/js/productsAdmin.js
+++ src/public/js/productsAdmin.js
@@ -38,31 +38,6 @@ document.addEventListener('DOMContentLoaded', function (){
             }
         });
     });
-        const addToCartButtons = document.querySelectorAll('button[productId]');
-        const cartId = document.getElementById('cartId').value;
-        addToCartButtons.forEach(function (button) {
-            button.addEventListener('click', function () {
-                const productId = button.getAttribute('productId');
-                const quantityInputId = `#quantityProduct_${productId}`;
-                const quantity = document.querySelector(quantityInputId).value;
-                fetch(`/api/carts/${cartId}/products/${productId}`, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({ quantity: quantity }),
-                }).then(response => {
-                    if (response.ok) {
-                        console.log(`Product with ID ${productId} added to the cart for user ${cartId}`);
-                    } else {
-                        console.error('Error adding product to the cart');
-                    }
-                }).catch(error => {
-                    console.error('Error:', error);
-                });
-            });
-        });
- 
 
     // Edit Product Buttons
     const editProductButtons = document.querySelectorAll('button#editButton');
