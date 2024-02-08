99edbb3b64a05efee73a2c876d3a5cd151b295fc
Eric Negreido <ericnegreidooo@gmail.com>
2024-02-07 22:29:27 -0300

    port

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ import {__mainDirname} from './utils.js'
 import handleConnection from './chatApp/chat.js';
 const app = express();
 
-const PORT =  8080;
+const PORT =  process.env || 8080;
 
 const swaggerOptions = {
     definition:{
